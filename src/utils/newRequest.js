dea44ed5f7067b98b35c86c038992e68685d8f30
thanhcong <thanhcong1506@gmail.com>
2023-04-23 10:44:32 +0700

    commit

---
+++ src/utils/newRequest.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const newRequest = axios.create({
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecommerce-utm9.onrender.com/api",
+  crossDomain: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+  },
+  credentials: "include",
+  withCredentials: true,
+});
+
+export default newRequest;
